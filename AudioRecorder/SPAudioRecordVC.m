c4a1e28b6aa4a364b97f40d063200eaaccd76108
Azat Minvaliev <Miatozoff@gmail.com>
2015-03-11 09:50:31 +0200

    IOS-4178 #Done!

---
+++ AudioRecorder/SPAudioRecordVC.m
@@ -0,0 +1,21 @@
+//
+//  SPAudioRecordVC.m
+//  AudioRecorder
+//
+//  Created by Азат on 11.03.15.
+//  Copyright (c) 2015 Azat Minvaliev. All rights reserved.
+//
+
+#import "SPAudioRecordVC.h"
+
+@implementation SPAudioRecordVC
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
